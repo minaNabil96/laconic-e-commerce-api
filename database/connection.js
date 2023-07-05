373c422993a7df9fce94553c0d56ccb657c46ae8
minaNabil96 <minanabilfahmy96@gmail.com>
2023-07-05 15:10:04 +0300

    try1

--- database/connection.js
+++ database/connection.js
@@ -2,14 +2,17 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const mongoUrl = process.env.MONGODB_URL;
-main();
 // .catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect(`${mongoUrl}`).then((resault) => {
-    console.log("connect sucsses");
-  });
-  // .catch((err) => {
-  //   console.log(err);
-  // });
+  await mongoose
+    .connect(`${mongoUrl}`)
+    .then((resault) => {
+      console.log("connect sucsses");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
+
+main();
